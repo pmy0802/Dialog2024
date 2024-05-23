07b36a5c97b426c3d7d44af9c88444f673a56486
pmy0802 <pmy0802@naver.com>
2024-05-23 16:15:25 +0900

    기본목록대화상장 완성

--- app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
@@ -14,14 +14,16 @@ class MainActivity : AppCompatActivity() {
         var btnDialog = findViewById<Button>(R.id.btnDialog)
 
         btnDialog.setOnClickListener{
+            // 기본 목록 대화상자
+            var placesArr = arrayOf("에펠탑", "빅벤", "피렌체")
             var dialog = AlertDialog.Builder(this@MainActivity)
-            dialog.setTitle("안내메시지")
-            dialog.setMessage("오늘은 목요일입니다.")
+            dialog.setTitle("좋아하는 핫플레이스")
             dialog.setIcon(R.drawable.icon1)
-            dialog.setPositiveButton("확인"){dialog1, which ->
-                Toast.makeText(this@MainActivity, "확인 버튼을 클릭했습니다.", Toast.LENGTH_SHORT).show()
+            dialog.setItems(placesArr){ d1, which ->
+                btnDialog.text = placesArr[which]
 
             }
+            dialog.setPositiveButton("닫기", null)
             dialog.show()
 
         }
