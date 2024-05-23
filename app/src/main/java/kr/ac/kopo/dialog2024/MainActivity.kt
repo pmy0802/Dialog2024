3ff08164da8a97f884c17d2a74da3268d5c22ebc
pmy0802 <pmy0802@naver.com>
2024-05-23 16:21:42 +0900

    라디오버튼 목록 대화상자 완성

--- app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
@@ -14,12 +14,13 @@ class MainActivity : AppCompatActivity() {
         var btnDialog = findViewById<Button>(R.id.btnDialog)
 
         btnDialog.setOnClickListener{
-            // 기본 목록 대화상자
+            // 라디오 버튼 목록 대화상자
             var placesArr = arrayOf("에펠탑", "빅벤", "피렌체")
+            var selectArr = booleanArrayOf(false, true, false)
             var dialog = AlertDialog.Builder(this@MainActivity)
             dialog.setTitle("좋아하는 핫플레이스")
             dialog.setIcon(R.drawable.icon1)
-            dialog.setItems(placesArr){ d1, which ->
+            dialog.setSingleChoiceItems(placesArr, 1){ d1, which ->
                 btnDialog.text = placesArr[which]
 
             }
